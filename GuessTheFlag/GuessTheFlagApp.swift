102c3e05e74d104565d1a6b720a393f686edf03a
Michael Mahaffey <mahaffeymg@me.com>
2023-06-11 14:24:33 -0400

    First Commit

---
+++ GuessTheFlag/GuessTheFlagApp.swift
@@ -0,0 +1,17 @@
+//
+//  GuessTheFlagApp.swift
+//  GuessTheFlag
+//
+//  Created by Mahaffey Dev on 6/9/23.
+//
+
+import SwiftUI
+
+@main
+struct GuessTheFlagApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
